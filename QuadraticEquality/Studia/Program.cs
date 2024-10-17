897126f7e64d470092a5aff470659a50d599cf08
Maciej Broda <maciejbroda01@gmail.com>
2024-10-17 19:51:19 +0200

    QuadraticEquality

---
+++ QuadraticEquality/Studia/Program.cs
@@ -0,0 +1,13 @@
+﻿using Studia;
+
+QuadraticEq.QuadraticEquation(1, 3, 2);
+Console.WriteLine();
+QuadraticEq.QuadraticEquation(1, -2, 1);
+Console.WriteLine();
+QuadraticEq.QuadraticEquation(1, 1, 1);
+Console.WriteLine();
+QuadraticEq.QuadraticEquation(0, 0, 0);
+Console.WriteLine();
+QuadraticEq.QuadraticEquation(0, 0, 1);
+Console.WriteLine();
+QuadraticEq.QuadraticEquation(0, 2, 1);
---
+++ QuadraticEquality/Studia/QuadraticEquation.cs
@@ -0,0 +1,58 @@
+﻿using System.Globalization;
+
+namespace Studia
+{
+    public class QuadraticEq
+    {
+
+
+        public static void QuadraticEquation(int a, int b, int c)
+        {
+
+            CultureInfo.CurrentCulture = new CultureInfo("en-US");
+
+            if (a == 0 && b == 0 && c == 0)
+            {
+                Console.WriteLine("infinity");
+                return;
+            }
+
+            double x1;
+            double x2;
+
+            double delta = (b * b) - (4 * a * c);
+            double pierwiastekZDelty = Math.Sqrt(delta);
+
+            double aRazyDwa = 2 * a;
+
+            if (delta < 0 || aRazyDwa == 0)
+            {
+                Console.WriteLine("empty");
+                return;
+            }
+
+            else if (delta > 0)
+            {
+
+                x1 = (-b - pierwiastekZDelty) / (aRazyDwa);
+
+                x2 = (-b + pierwiastekZDelty) / (aRazyDwa);
+
+                x1 = Math.Round(x1, 2);
+                x2 = Math.Round(x2, 2);
+
+                Console.WriteLine($"{nameof(x1)}={x1:F2}");
+                Console.WriteLine($"{nameof(x2)}={x2:F2}");
+            }
+            else if (delta == 0)
+            {
+                x1 = -b / (aRazyDwa);
+
+                x1 = Math.Round(x1, 2);
+
+                Console.WriteLine($"x={x1:F2}");
+            }
+
+        }
+    }
+}
