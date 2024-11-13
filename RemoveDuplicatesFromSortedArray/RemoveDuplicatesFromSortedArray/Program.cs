c3060eaa012b6b6a19dd68829ce69ca623a257a7
Maciej Broda <maciejbroda01@gmail.com>
2024-11-13 15:04:03 +0100

    Resolving a problem from leetcode

---
+++ RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray/Program.cs
@@ -0,0 +1,7 @@
+﻿using RemoveDuplicatesFromSortedArray;
+
+var solution = new Solution();
+solution.RemoveDuplicates([1, 1, 2]);
+
+Solution solution1 = new();
+solution1.RemoveDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]);
---
+++ RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace RemoveDuplicatesFromSortedArray
+{
+    public class Solution
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            int k = 1;
+
+            if (nums.Length == 0)
+            {
+                return 0;
+            }
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] != nums[i - 1])
+                {
+                    nums[k] = nums[i];
+                    k++;
+                }
+
+            }
+            return k;
+        }
+    }
+}
