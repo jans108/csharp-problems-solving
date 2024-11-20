e9af2bb9db15297d24a89231ea4b5813b79ee2ac
Maciej Broda <maciejbroda01@gmail.com>
2024-11-20 15:05:18 +0100

    Solving Leetcode task

---
+++ RemoveDuplicatesFromSortedArrayII/RemoveDuplicatesFromSortedArrayII/Program.cs
@@ -0,0 +1,7 @@
+﻿using RemoveDuplicatesFromSortedArrayII;
+
+Solution solution = new();
+
+int[] nums = { 1, 1, 1, 2, 2, 3 };
+
+solution.RemoveDuplicates(nums);
---
+++ RemoveDuplicatesFromSortedArrayII/RemoveDuplicatesFromSortedArrayII/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace RemoveDuplicatesFromSortedArrayII;
+
+public class Solution
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+       if (nums.Length < 3)
+        {
+            return nums.Length;
+        }
+
+        int k = 2;
+
+        for (int i = 2; i < nums.Length; i++)
+        {
+            if (nums[i] != nums[k - 2])
+            {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+}
