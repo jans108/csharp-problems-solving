7330255dfed9cd2217742b22d6a788922aa6788f
Maciej Broda <maciejbroda01@gmail.com>
2024-11-21 20:33:02 +0100

    Solving leetcode task

---
+++ MajorityElement/MajorityElement/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace MajorityElement;
+
+public class Solution
+{
+    public int MajorityElement(int[] nums)
+    {
+        int candidate = nums[0];
+
+        int counter = 1;
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (counter == 0)
+            {
+                candidate = nums[i];
+                counter = 1;
+            }
+            else if (nums[i] == candidate)
+            {
+                counter++;
+            }
+            else
+            {
+                counter--;
+            }
+
+        }
+
+        return candidate;
+    }
+}
